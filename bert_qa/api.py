31827a0a884a2c12bece1227ed6c8efcac5461d0
bhperry <ben@saturncloud.io>
2023-04-27 19:30:56 +0000

    optional section

--- bert_qa/api.py
+++ bert_qa/api.py
@@ -1,13 +1,21 @@
+from dataclasses import dataclass
 import pathlib
+from typing import Optional
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse, FileResponse
 import uvicorn
 
-from bert_qa.model import BertQA, Question
+from bert_qa.model import BertQA
 
 app = FastAPI()
 bert_qa = BertQA()
+
+
+@dataclass
+class QuestionBody:
+    question: str
+    section: Optional[str] = None
 
 
 @app.get("/")
@@ -16,8 +24,8 @@ def index():
 
 
 @app.post("/api/question")
-def post_question(question: Question):
-    answer = bert_qa.search_docs(question, span_length=384, span_overlap=64)
+def post_question(body: QuestionBody):
+    answer = bert_qa.search_docs(body.question, body.section, span_length=384, span_overlap=64)
     return JSONResponse(answer.to_dict())
 
 
--- bert_qa/model.py
+++ bert_qa/model.py
@@ -1,16 +1,10 @@
 from dataclasses import asdict, dataclass
-from typing import Any, Dict, Iterable, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple
 
 import torch
 from transformers import BatchEncoding, BertTokenizerFast, BertForQuestionAnswering
 
-from bert_qa.docs import DOCS_DIR, load_docs
-
-
-@dataclass
-class Question:
-    text: str
-    section: str = "concepts"
+from bert_qa.docs import load_docs
 
 
 @dataclass
@@ -26,6 +20,14 @@ class Answer:
     def to_dict(self) -> Dict[str, Any]:
         return asdict(self)
 
+    def print(self):
+        print(
+            f"Question: {self.question}",
+            f"\nAnswer: {self.text}",
+            f"\nSource: {self.source}",
+            f"\nScore: {self.score}\n",
+        )
+
 
 class BertQA:
     def __init__(self):
@@ -33,19 +35,28 @@ class BertQA:
         self.tokenizer: BertTokenizerFast = BertTokenizerFast.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
         self.model: BertForQuestionAnswering = BertForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
 
-    def search_docs(self, question: Question, span_length: int = 512, span_overlap: int = 128) -> Answer:
-        # Only searching the concepts section for now
+    def search_docs(self, question: str, section: Optional[str] = None, span_length: int = 512, span_overlap: int = 128) -> Answer:
+        if not section:
+            best_section_score = -1e20
+            best_section = ""
+            sections = self.docs.keys()
+            for section in sections:
+                _, score = self.answer_question(question, section)
+                if score > best_section_score:
+                    best_section_score = score
+                    best_section = section
+            section = best_section
+
         best_score = -1e20
         best_answer = ""
         best_source = ""
-
-        for file, context in self.docs[question.section].items():
-            answer, score = self.answer_question(question.text, context, span_length=span_length, span_overlap=span_overlap)
+        for file, context in self.docs[section].items():
+            answer, score = self.answer_question(question, context, span_length=span_length, span_overlap=span_overlap)
             if answer and score > best_score:
                 best_answer = answer
                 best_source = file
                 best_score = score
-        return Answer(best_answer, question.text, best_source, best_score)
+        return Answer(best_answer, question, best_source, best_score)
 
     def answer_question(
         self,

dc019ee87ae820e10898551ac271cd2b9d5a73c1
bhperry <ben@saturncloud.io>
2023-06-07 17:02:08 +0000

    Update streamlit app to not rely on session state

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,3 +1,4 @@
+from typing import List
 import streamlit as st
 
 from bert_qa.fastchatter import StreamlitChatLoop
@@ -6,84 +7,67 @@ st.set_page_config(page_title="pubmed chat", page_icon=":robot_face:", layout='w
 
 @st.cache_resource
 def get_chatter():
-    chatter = StreamlitChatLoop()
-    chatter.load_models()
-    return chatter
-
-
-# if 'chatter' not in st.session_state:
-#     st.session_state['chatter'] = StreamlitChatLoop()
-#     st.session_state['chatter'].load_models()
-#
-# chatter = st.session_state['chatter']
+    return StreamlitChatLoop()
 
 chatter = get_chatter()
 
 print('GOO')
 
 output = st.text('')
+included: List[bool] = []
 
 def clear_conversation():
-    chatter.conv = None
-    st.session_state.pop('user_input')
-    chatter.question = None
-    chatter.results = None
-    chatter.system = None
+    global included, user_input
+    chatter.conv.system = ""
+    chatter.conv.messages = []
+    chatter.results = []
+    included = []
 
-st.button("clear conversation", on_click=clear_conversation)
-
-
-with st.form(key='my_form', clear_on_submit=True):
-    user_input = st.text_area("You:", key='input', height=100)
-    submit_button = st.form_submit_button(label='Send')
-    st.session_state['user_input'] = user_input
-
-if not st.session_state.get('user_input') or st.session_state.get('last_input') == st.session_state.get('user_input'):
-    st.stop()
-
-if chatter.conv is None:
-    chatter.set_question(st.session_state['user_input'])
-    chatter.make_prompt(chatter.results)
-    chatter.create_conversation()
-
-
-include_doc = []
 
 def apply_filter():
     print('FILTER')
     docs = []
-    for doc, to_include in zip(chatter.results, include_doc):
+    for doc, to_include in zip(chatter.results, included):
         if to_include:
             docs.append(doc)
     chatter.make_prompt(docs)
     chatter.conv.messages = []
-    chatter.conv.system = chatter.system
-    st.session_state['user_input'] = chatter.question
+    chatter.append_question(user_input)
 
+clear_convo = st.button("clear conversation", on_click=clear_conversation)
 
-with st.form(key='checklists'):
-    for idx, doc in enumerate(chatter.results):
-        include = st.checkbox('include in chat context', key=idx, value=True)
-        include_doc.append(include)
-        st.write(f"Title - {doc.title}")
-        st.write(f"Publication - {doc.meta['publication_title']}")
-        # text = doc.text.replace('\n', '')
-        text = doc.original_text
-        # print(idx, text)
-        st.write(f"Text - {text}")
-        st.divider()
-    print(include_doc)
-    submit_button = st.form_submit_button(label='Filter', on_click=apply_filter)
+with st.form(key='my_form', clear_on_submit=True):
+    user_input = st.text_area("You:", key='input', height=100)
+    question_submit_button = st.form_submit_button(label='Send')
 
+if question_submit_button and not clear_convo:
+    chatter.append_question(user_input)
+    output.write(chatter.get_chat())
+    with st.spinner("Searching..."):
+        chatter.search_results(user_input)
 
-chatter.start_loop(st.session_state['user_input'])
-messages = [f'{role}: {"" if message is None else message}' for role, message in chatter.conv.messages]
-message_string = "\n\n".join(messages)
-for text in chatter.loop():
-    message = message_string + text
-    message = message.replace('\n', '\n\n')
-    output.write(message)
+if chatter.results:
+    with st.form(key='checklists'):
+        included = []
+        for idx, doc in enumerate(chatter.results):
+            include = st.checkbox('include in chat context', key=idx, value=True)
+            included.append(include)
+            st.write(f"Title - {doc.title}")
+            st.write(f"Publication - {doc.meta['publication_title']}")
+            # text = doc.text.replace('\n', '')
+            text = doc.original_text
+            st.write(f"Text - {text}")
+            st.divider()
+        checklist_submit_button = st.form_submit_button(label='Filter')
 
-st.session_state['last_input'] = st.session_state['user_input']
+    if checklist_submit_button:
+        apply_filter()
+else:
+    checklist_submit_button = False
 
-
+if (question_submit_button or checklist_submit_button) and not clear_convo:
+    message_string = chatter.get_chat()
+    for text in chatter.loop():
+        message = message_string + text
+        message = message.replace('\n', '\n\n')
+        output.write(message)

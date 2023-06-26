f7773b08b8c18777235637a2151542265566f3c0
bhperry <ben@saturncloud.io>
2023-06-26 23:12:17 +0000

    Support for querying existing contexts

--- llm/qa/streamlit/app.py
+++ llm/qa/streamlit/app.py
@@ -47,16 +47,14 @@ def get_qa_session(engine: QueuedEngine, vector_store: VectorStore) -> QASession
     return st.session_state["qa_session"]
 
 
-def apply_filter():
-    # Filter which contexts are seen by the LLM, and re-ask the last question
+def filter_contexts():
+    # Filter which contexts are seen by the LLM for the next question
     contexts = []
     for doc, to_include in zip(qa_session.results, included):
         if to_include:
             contexts.append(doc.page_content)
 
-    qa_session.clear(keep_results=True)
     qa_session.set_context(contexts)
-    qa_session.append_question(user_input)
 
 
 engine = get_inference_engine()
@@ -68,39 +66,50 @@ included: List[bool] = []
 clear_convo = st.button("clear conversation", on_click=qa_session.clear)
 
 with st.form(key="input_form", clear_on_submit=True):
+    # Collect user input
     user_input = st.text_area("You:", key="input", height=100)
-    question_submit_button = st.form_submit_button(label="Send")
-    if question_submit_button and not user_input:
-        question_submit_button = False
+    query_submitted = st.form_submit_button(label="Query")
+    if query_submitted or qa_session.results:
+        query_existing_submitted = st.form_submit_button(label="Query Existing")
+    else:
+        query_existing_submitted = False
 
-if question_submit_button and not clear_convo:
+    if query_existing_submitted:
+        query_submitted = True
+    if query_submitted and not user_input:
+        query_submitted = False
+
+if query_submitted and not clear_convo:
     # Write question out to streamlit, then search for contexts
     qa_session.append_question(user_input)
     output.write(qa_session.get_history())
-    with st.spinner("Searching..."):
-        qa_session.update_context(user_input)
+    if not query_existing_submitted:
+        with st.spinner("Searching..."):
+            qa_session.update_context(user_input)
 
 if qa_session.results:
     # Write contexts out to streamlit, with checkboxes to filter what is sent to the LLM
     with st.form(key="checklists"):
-        included = []
-        for idx, doc in enumerate(qa_session.results):
-            include = st.checkbox("include in chat context", key=idx, value=True)
+        for i, doc in enumerate(qa_session.results):
+            include = st.checkbox("include in chat context", key=i, value=True)
             included.append(include)
             st.write(doc.page_content)
             st.write({k: v for k, v in doc.metadata.items() if k != DataFields.EMBEDDING})
             st.divider()
-        checklist_submit_button = st.form_submit_button(label="Filter")
 
-    if checklist_submit_button:
-        apply_filter()
-else:
-    checklist_submit_button = False
+        checklist_submitted = st.form_submit_button(label="Filter")
+        if checklist_submitted:
+            filter_contexts()
 
-if (question_submit_button or checklist_submit_button) and not clear_convo:
-    # Stream response from LLM, updating chat window at each step
-    message_string = qa_session.get_history()
-    for text in qa_session.conversation_stream():
-        message = message_string + text
-        message = message.replace("\n", "\n\n")
+if not clear_convo:
+    if query_submitted:
+        # Stream response from LLM, updating chat window at each step
+        message_string = qa_session.get_history()
+        for text in qa_session.conversation_stream():
+            message = message_string + text
+            message = message.replace("\n", "\n\n")
+            output.write(message)
+    else:
+        # Write existing message history
+        message = qa_session.get_history().replace("\n", "\n\n")
         output.write(message)

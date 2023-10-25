892fe7846cb76b591a8e311a2bca7381659a261c
bhperry <ben@saturncloud.io>
2023-10-25 22:24:58 +0000

    use default model padding-side, unk token for llama2-chat padding

--- llm/model_configs.py
+++ llm/model_configs.py
@@ -183,10 +183,6 @@ class VicunaConfig(LlamaBaseConfig):
     model_id: str = "lmsys/vicuna-7b-v1.5"
     max_length: int = 4096
     format: PromptFormat = field(default_factory=VicunaFormat)
-    tokenizer_kwargs: Dict[str, Any] = field(default_factory=lambda: {
-        # Improves batch inference performance on inputs of different length
-        "padding_side": "left",
-    })
 
 
 VicunaConfig.register(
@@ -214,6 +210,11 @@ class Llama2ChatConfig(LlamaBaseConfig):
     model_id: str = "meta-llama/Llama-2-7b-chat-hf"
     max_length: int = 4096
     format: PromptFormat = field(default_factory=Llama2Format)
+    tokenizer_kwargs: Dict[str, Any] = field(default_factory=lambda: {
+        # Llama2 does not have a pad token. For batched inference, using the unk_token
+        # performs better than the standard recommendation of using eos_token
+        "pad_token": "<unk>",
+    })
 
 
 Llama2ChatConfig.register(
